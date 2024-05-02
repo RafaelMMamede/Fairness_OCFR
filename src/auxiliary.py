62d3bd5034ee27bc8f2dd1de06569d1d9665f7c8
Rafael Mamede <rafaelcmpmamede@gmail.com>
2024-05-02 11:09:07 +0100

    updated auxiliary with eer_th calculation routines

--- src/auxiliary.py
+++ src/auxiliary.py
@@ -10,6 +10,11 @@ sys.path.append('../../ElasticFace') #REPLACE WITH PATH TO ELASTICFACE
 import cv2
 from backbones.iresnet import iresnet34, iresnet50
 
+from scipy.optimize import brentq
+from scipy.interpolate import interp1d
+from sklearn.metrics import roc_curve
+import sklearn as sk
+
 
 def create_model(model,path,device):
     """
@@ -46,3 +51,56 @@ def preprocess_image(image_path):
     input_tensor = input_tensor.to(torch.float)
     input_batch = input_tensor.unsqueeze(0)  # Add a batch dimension
     return input_batch
+
+
+
+def open_results(dataset):
+    """
+    Get dictionary with similarities and array of matches
+    :param dataset (str): Dataset from which to get results. From RFW, RFW_unaligned, RFW_protocolo1, RFW_protocolo1_unaligned, RFW_protocolo4, RFW_protocolo4_unaligned
+    """
+    ethnicities = ['African', 'Asian','Caucasian','Indian']
+    similarities = {}
+    matches = {}
+
+
+    for eth in ethnicities:
+        with  open('../results/' + dataset + '/' + eth + '/cos_sim.json') as user_file:
+            similarities[eth] = json.load(user_file)
+        matches[eth] = np.load('../results/' + dataset + '/' + eth + '/matches.npy')
+
+    return similarities, matches
+
+
+
+def EER_acc(matches,similarity_prediction):
+    """
+    Get Accuracy @ Equal Error Rate Threshold 
+    :param matches:  true labels
+    :param similarity_predictions: cossine similarity between embeddings
+    """
+
+    fpr, tpr, thresholds = roc_curve(matches, similarity_prediction, pos_label=1)
+
+    eer = brentq(lambda x : 1. - x - interp1d(fpr, tpr)(x), 0., 1.)
+    thresh = interp1d(fpr, thresholds)(eer)
+
+    pred_label = np.where(similarity_prediction>=thresh,1,0)
+    acc = sk.metrics.accuracy_score(matches, pred_label)
+    return acc
+
+
+
+def EER_th(matches,similarity_prediction):
+    """
+    Get Equal Error Rate Threshold 
+    :param matches:  true labels
+    :param similarity_predictions: cossine similarity between embeddings
+    """
+
+    fpr, tpr, thresholds = roc_curve(matches, similarity_prediction, pos_label=1)
+
+    eer = brentq(lambda x : 1. - x - interp1d(fpr, tpr)(x), 0., 1.)
+
+    thresh = interp1d(fpr, thresholds)(eer)
+    return thresh
