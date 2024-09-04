4478c98c5d9e7a905595ac44cb0656a2b50c494e
Rafael Mamede <rafaelcmpmamede@gmail.com>
2024-09-04 13:47:36 +0100

    added workstation shell files

---
+++ src/extract_overlaps.sh
@@ -0,0 +1,9 @@
+python extract_overlaps.py
+python extract_overlaps.py --importance_mode positive_contributions
+python extract_overlaps.py --importance_mode negative_contributions
+
+python extract_overlaps.py --dataset_name RFW0_RFW4 --occlusion_paths_R ../../../Datasets/RFW_occ_protocolo4/African/_mask/,../../../Datasets/RFW_occ_protocolo4/Asian/_mask/,../../../Datasets/RFW_occ_protocolo4/Caucasian/_mask/,../../../Datasets/RFW_occ_protocolo4/Indian/_mask/ 
+python extract_overlaps.py --dataset_name RFW0_RFW4 --importance_mode positive_contributions --occlusion_paths_R ../../../Datasets/RFW_occ_protocolo4/African/_mask/,../../../Datasets/RFW_occ_protocolo4/Asian/_mask/,../../../Datasets/RFW_occ_protocolo4/Caucasian/_mask/,../../../Datasets/RFW_occ_protocolo4/Indian/_mask/
+python extract_overlaps.py --dataset_name RFW0_RFW4 --importance_mode negative_contributions --occlusion_paths_R ../../../Datasets/RFW_occ_protocolo4/African/_mask/,../../../Datasets/RFW_occ_protocolo4/Asian/_mask/,../../../Datasets/RFW_occ_protocolo4/Caucasian/_mask/,../../../Datasets/RFW_occ_protocolo4/Indian/_mask/
+
+
